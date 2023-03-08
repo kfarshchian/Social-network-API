af08841fdcb456a55e744f97506941ffb9eaad04
Kameron <kfarshchian@gmail.com>
2023-03-08 14:32:35 -0700

    update models and some routes

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: 'User name is required',
+    },
+    email: {
+      type: String,
+      required: 'Email address is required',
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address'],
+    },
+    thoughts:[ {
+      type: Schema.Types.ObjectID,
+      ref: 'Thought',
+      },
+     ],
+     friends:[ {
+      type: Schema.Types.ObjectID,
+      ref: 'User',
+      },
+     ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtuals('friendCount').get(function(){
+  return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/Thought', thoughtRoutes);
+router.use('/User', userRoutes);
 
 module.exports = router;
